3fd3f97645e176227ec6a699ddfd741705c954f2
Betoml5 <betoml5@hotmail.com>
2022-03-08 14:07:59 -0600

    Add example chart

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,58 @@
 import React, { useEffect, useState } from "react";
-
+import { Bar, Line } from "react-chartjs-2";
 import User from "../components/User";
-
+import Chart from "chart.js/auto";
 const Home = () => {
-  const labels = ["Usuarios", "Personas"];
-
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
+  const data = {
+    labels,
+    datasets: [
+      {
+        label: "Dataset 1",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(255, 99, 132)",
+        backgroundColor: "rgba(255, 99, 132, 0.5)",
+      },
+      {
+        label: "Dataset 2",
+        data: labels.map(() =>
+          faker.datatype.number({ min: -1000, max: 1000 })
+        ),
+        borderColor: "rgb(53, 162, 235)",
+        backgroundColor: "rgba(53, 162, 235, 0.5)",
+      },
+    ],
+  };
   return (
     <div className=" ">
-      <User />
-      <User />
-      <User />
-      <User />
+      <Line
+        datasetIdKey="id"
+        data={{
+          labels: ["Jun", "Jul", "Aug"],
+          datasets: [
+            {
+              id: 1,
+              label: "",
+              data: [5, 6, 7],
+            },
+            {
+              id: 2,
+              label: "",
+              data: [3, 2, 1],
+            },
+          ],
+        }}
+      />
     </div>
   );
 };
