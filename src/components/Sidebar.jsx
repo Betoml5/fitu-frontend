dc2b2063bfebf635ee61b4b201d5fb6b46663673
Betoml5 <betoml5@hotmail.com>
2022-03-09 14:10:18 -0600

    Add sidebar animation

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,23 +1,42 @@
-import React from "react";
-import { AiFillHome } from "react-icons/ai";
+import React, { useState } from "react";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
 import { BsFillPeopleFill } from "react-icons/bs";
 import { FaUserPlus } from "react-icons/fa";
 import { IoMdSettings } from "react-icons/io";
 import { Link } from "react-router-dom";
 const Sidebar = () => {
+  const [open, setOpen] = useState(false);
   return (
     <div
-      className={`h-screen bg-[#141E27] text-white relative transition-all duration-700 ease-in-out p-4  `}
+      className={` ${
+        open ? "w-56" : "w-20"
+      } duration-500  bg-primary text-white sticky p-4  `}
     >
-      <div className="flex flex-col items-center justify-between h-1/2 ">
-        <Link to="/">
+      <div>
+        <AiOutlineMenu
+          size={30}
+          onClick={() => setOpen(!open)}
+          className={`duration-500 ${
+            open && "rotate-[360deg]"
+          }  cursor-pointer`}
+        />
+
+        <Link to="/" className="sideBarItem">
           <AiFillHome size={30} />
+          <p className={`${!open && "hidden"} sideBarItemText `}>Inicio</p>
         </Link>
-        <Link to="/clientes/nuevo">
+        <Link to="/clientes/nuevo" className="sideBarItem">
           <FaUserPlus size={30} />
+          <p className={`${!open && "hidden"} sideBarItemText `}>Nuevo</p>
         </Link>
-        <BsFillPeopleFill size={30} />
-        <IoMdSettings size={30} />
+        <Link to="/clientes/nuevo" className="sideBarItem">
+          <BsFillPeopleFill size={30} />
+          <p className={`${!open && "hidden"} sideBarItemText `}>Clientes</p>
+        </Link>
+        <Link to="/clientes/nuevo" className="sideBarItem">
+          <IoMdSettings size={30} />
+          <p className={`${!open && "hidden"} sideBarItemText `}>Ajustes</p>
+        </Link>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -30,13 +30,13 @@ const Home = () => {
 
   return (
     <div className="w-full max-w-3xl p-4">
-      <h3 className="text-white text-xl  my-4">Clientes</h3>
+      <h3 className="text-white text-xl my-4">Clientes</h3>
       <section className="flex gap-1 text-white">
-        <div className="bg-primary p-4 py-10  rounded-lg w-full">
-          <p className="text-4xl font-bold">14</p>
-          <h3 className="font-semibold text-xl">Clientes nuevos</h3>
+        <div className="bg-primary p-4 py-10  rounded-lg w-1/2">
+          <p className="text-2xl font-bold">14</p>
+          <h3 className="font-semibold text-lg">Clientes nuevos</h3>
         </div>
-        <div className="bg-strongBlue p-4 py-10  rounded-lg w-full">
+        <div className="bg-strongBlue p-4 py-10  rounded-lg w-1/2">
           <p className="text-4xl font-bold">29</p>
           <h3 className="font-semibold text-xl">Clientes pendientes</h3>
         </div>
@@ -68,61 +68,6 @@ const Home = () => {
         <h3 className="text-white">Clientes nuevos</h3>
 
         <Bar
-          width={340}
-          options={{
-            maintainAspectRatio: false,
-            scales: {
-              y: {
-                beginAtZero: true,
-              },
-            },
-          }}
-          data={{
-            labels: [
-              "Lunes",
-              "Martes",
-              "Miercoles",
-              "Jueves",
-              "Viernes",
-              "Sabado",
-              "Domingo",
-            ],
-            datasets: [
-              {
-                label: "Numero de clientes",
-                data: [12, 19, 3, 5, 2, 3, 10],
-                backgroundColor: [
-                  "rgba(255, 99, 132, 0.2)",
-                  "rgba(54, 162, 235, 0.2)",
-                  "rgba(255, 206, 86, 0.2)",
-                  "rgba(75, 192, 192, 0.2)",
-                  "rgba(153, 102, 255, 0.2)",
-                  "rgba(255, 159, 64, 0.2)",
-                ],
-                borderColor: [
-                  "rgba(255, 99, 132, 1)",
-                  "rgba(54, 162, 235, 1)",
-                  "rgba(255, 206, 86, 1)",
-                  "rgba(75, 192, 192, 1)",
-                  "rgba(153, 102, 255, 1)",
-                  "rgba(255, 159, 64, 1)",
-                ],
-                borderWidth: 1,
-              },
-              // {
-              //   label: "Quantity",
-              //   data: [100, 103, 67, 56, 50, 50],
-              //   backgroundColor: "orange",
-              //   borderColor: "red",
-              // },
-            ],
-          }}
-        />
-      </div>
-
-      <div className="w-3/4 h-2/4 p-2 mt-10">
-        <h3 className="text-white">Clientes pendientes</h3>
-        <Doughnut
           width={340}
           options={{
             maintainAspectRatio: false,
