22fe822801be2342af243d7831c7e317bdc6a747
Betoml5 <betoml5@hotmail.com>
2022-03-23 09:00:59 -0600

    Remove unused code

--- src/pages/user/Dashboard.jsx
+++ src/pages/user/Dashboard.jsx
@@ -1,16 +1,16 @@
 import React from "react";
-import { useCookies } from "react-cookie";
 import { GiPencilRuler } from "react-icons/gi";
 import useUser from "../../hooks/useUser";
 
 const Dashboard = () => {
   const { user } = useUser();
-  console.log(user);
 
   return (
     <div className="w-full max-w-3xl mx-auto text-white p-4">
       <h3 className="text-xl font-semibold">Dashboard</h3>
-      <h4 className="my-4">Juan Perez</h4>
+      <h4 className="my-4">
+        {user.firstName} {user.lastName}
+      </h4>
 
       <section className="grid gap-y-2 md:grid-cols-2 md:gap-y-4 md:gap-x-4 ">
         <section className="relative bg-strongBlue p-4 rounded-lg">
