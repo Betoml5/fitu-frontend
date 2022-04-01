1d696c5b71820ac8503443cd63de320b774f4867
Betoml5 <betoml5@hotmail.com>
2022-04-01 16:43:24 -0600

    Add inputs to meeting form

--- src/components/admin/MeetingForm.jsx
+++ src/components/admin/MeetingForm.jsx
@@ -14,11 +14,11 @@ const MeetingForm = () => {
 
   const { getCustomerDetails } = useAdmin();
 
+  console.log(customer);
+
   useEffect(() => {
     getCustomerDetails(id)
-      .then((data) => {
-        console.log(data);
-      })
+      .then((data) => setCustomer(data))
       .catch((error) => {
         console.log(error);
         throw error;
@@ -26,12 +26,52 @@ const MeetingForm = () => {
   }, []);
 
   return (
-    <div>
-      <div>
-        <form>
-          <label htmlFor=""></label>
-        </form>
-      </div>
+    <div className="w-full p-4">
+      <form className="flex flex-col max-w-3xl mx-auto">
+        <label htmlFor="firstName" className="text-white font-semibold text-lg">
+          Nombre
+        </label>
+        <input
+          type="text"
+          defaultValue={customer.firstName}
+          className="p-2 rounded-md mt-2"
+          readOnly
+        />
+        <label
+          htmlFor="lastName"
+          className="text-white font-semibold text-lg mt-4"
+        >
+          Apellidos
+        </label>
+        <input
+          type="text"
+          defaultValue={customer.lastName}
+          className="p-2 rounded-md mt-2"
+          readOnly
+        />
+        <label
+          htmlFor="phone "
+          className="text-white font-semibold text-lg mt-4"
+        >
+          Numero de telefono
+        </label>
+        <input
+          type="text"
+          defaultValue={customer.phone}
+          className="p-2 rounded-md mt-2"
+          readOnly
+        />
+        <label htmlFor="date" className="text-white font-semibold text-lg mt-4">
+          Fecha y hora
+        </label>
+        <input type="datetime-local" className="mt-4 p-4 rounded-md" />
+        <button
+          type="submit"
+          className="bg-strongBlue p-4 mt-4 rounded-lg text-white font-semibold hover:bg-opacity-90"
+        >
+          Crear cita
+        </button>
+      </form>
     </div>
   );
 };
